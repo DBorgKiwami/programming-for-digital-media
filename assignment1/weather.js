826230ae4bd1eebd5017d68fe5e7c57410be291a
DBorgKiwami <113467891+DBorgKiwami@users.noreply.github.com>
2025-11-28 23:48:18 +0000

    Added comments to weather.js

--- assignment1/weather.js
+++ assignment1/weather.js
@@ -1,5 +1,7 @@
+//Get DOM element
 const outputPar = document.getElementById("output");
 
+//Get data from weather API
 fetch("https://api.weatherapi.com/v1/current.json?key=49a4b9f01c4547d3a04211350250910&q=Dublin")
     .then((response) => {
         return response.json();
@@ -7,9 +9,11 @@ fetch("https://api.weatherapi.com/v1/current.json?key=49a4b9f01c4547d3a042113502
     .then((data) => {
         console.log(data)
         console.log(data.current.condition.text)
+        //Set DOM to recieved JSON data
         weatherInDublin = data.current.condition.text + ", " + data.current.temp_c + "°";
         outputPar.textContent = weatherInDublin
     })
     .catch((err) => {
         console.log(err);
+
     })
